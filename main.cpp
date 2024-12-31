cfe093bc9ed5fbb744b185f72cd7a0926ae1cd10
Neesarg Banglawala <neesarg@Neesargs-Mac-mini.local>
2024-12-31 00:50:46 -0800

    Adding VS Code Launch config such that 1. You can now use debugger 2. It runs the executable in external console (instead of VS Code output window which is read only)' ' '
    ç

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-#include "misc.h"
+#include "src/misc.h"
 
 int main()
 {
+	int input = -1;
+
+	std::cin >> input;
 	std::cout << "Hello, world!" << std::endl;
 	Misc::Print();
+
+	char c;
+	std::cin >> c;
 	return 0;
 }
